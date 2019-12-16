d24ab54d762c71486905e55225b6dda8ca3e2684
Ardhanari <genevril@gmail.com>
2019-12-16 02:08:12 +0000

    Project model created

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Project(models.Model):
+    img = models.ImageField(upload_to='images')
+    name = models.CharField(max_length=254, default='', blank=False)
+    linkGit = models.URLField(max_length=666, default='', blank=True)
+    linkDepl = models.URLField(max_length=666, default='', blank=True)
+    year = timezone.now
