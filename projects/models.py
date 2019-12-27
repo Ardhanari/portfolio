9583344daf60c37d3b4ebce95fe9cab7b656d209
Ardhanari <genevril@gmail.com>
2019-12-27 17:25:50 +0000

    change of folder files are uploaded to

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from datetime import datetime, timezone
 # Create your models here.
 
 class Project(models.Model):
-    img = models.ImageField(upload_to='images')
+    img = models.ImageField(upload_to='')
     name = models.CharField(max_length=254, default='', blank=False)
     linkGit = models.URLField(max_length=666, default='', blank=True)
     linkDepl = models.URLField(max_length=666, default='', blank=False)
