cec00e3b879aa52e5c7c934680c0e67078a4c88c
Ardhanari <genevril@gmail.com>
2019-12-28 19:18:55 +0000

    added animation to project links

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,8 +1,44 @@
+// $(selector).animate({params},speed,callback);
 
+// działa: 
+// $(".project").hover( function() {
+//     $(".project-row").css( "visibility", "visible" )
+// }, 
+//     function() {
+//         $(".project-row").css( "visibility", "collapse" )
+//     });
+
+// dziala:
+// $(".project").hover( function() {
+//         $(".project-row").animate({ opacity: "1"}, 10000)
+// })
+
+// // działa, ale działa na wszystkich jednocześnie
+// $(".project").hover(function(){
+// 	if (!$(".project-row").hasClass('animated')) {
+//         $(".project-row").dequeue().stop().animate({ opacity: "1" }, 300);
+// 	}
+// }, function() {
+//     $(".project-row").addClass('animated').animate({ opacity: "0" }, 300, function() {
+// 		$(".project-row").removeClass('animated').dequeue();
+// 	});
+// });
+
+
+// children() działa tylko raz? 
+// $(".project").hover(function(){
+// 	if (!$(this).children(".project-row").hasClass('animated')) {
+//         $(this).children(".project-row").dequeue().stop().animate({ opacity: "1" }, 300);
+// 	}
+// }, function() {
+//     $(this).children(".project-row").addClass('animated').animate({ opacity: "0" }, 300, function() {
+// 		$(this).children(".project-row").removeClass('animated').dequeue();
+// 	});
+// });
 
 $(".project").hover( function() {
-    $(".project-row").css( "visibility", "visible" )
+    $(this).children(".project-row").animate({ opacity: "1" }, 300).dequeue()
 }, 
     function() {
-        $(".project-row").css( "visibility", "collapse" )
+        $(this).children(".project-row").animate({ opacity: "0" }, 300).dequeue()
     });
