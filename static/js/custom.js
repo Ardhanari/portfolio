f8f7de32946cdd13eb48e3604b0e7c0b7dfb9067
Ardhanari <genevril@gmail.com>
2019-12-28 18:18:43 +0000

    added js script to projects

---
+++ static/js/custom.js
@@ -0,0 +1,8 @@
+
+
+$(".project").hover( function() {
+    $(".project-row").css( "visibility", "visible" )
+}, 
+    function() {
+        $(".project-row").css( "visibility", "collapse" )
+    });
