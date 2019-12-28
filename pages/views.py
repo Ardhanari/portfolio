c9ec259093b48a24ddaac5abdf833961add6493e
Ardhanari <genevril@gmail.com>
2019-12-28 22:49:21 +0000

    added model for cv file

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from projects.models import Project
+from projects.models import Project, Cv
 
 # Create your views here.
 def index(request): 
     projects = Project.objects.all()
-    return render(request, 'index.html', {'projects': projects})
+    try:
+        cv = Cv.objects.latest("date")
+        return render(request, 'index.html', {'projects': projects, 'cv': cv})
+    except:
+        return render(request, 'index.html', {'projects': projects})
--- projects/models.py
+++ projects/models.py
@@ -6,10 +6,17 @@ from datetime import datetime, timezone
 
 class Project(models.Model):
     img = models.ImageField(upload_to='')
-    name = models.CharField(max_length=254, default='', blank=False)
+    name = models.CharField(max_length=100, default='', blank=False)
     linkGit = models.URLField(max_length=666, default='', blank=True)
     linkDepl = models.URLField(max_length=666, default='', blank=False)
     year = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+class Cv(models.Model):
+    file = models.FileField(upload_to='')
+    date = models.DateField(blank=False, null=True, auto_now=True)
+
+    def __str__(self):
+        return "{0} CV".format(self.date)
