df74a1c788ef7c2db9b398cc0e4bf67932210c36
Ardhanari <genevril@gmail.com>
2019-12-28 22:58:50 +0000

    better file structure for uploaded media

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from datetime import datetime, timezone
 # Create your models here.
 
 class Project(models.Model):
-    img = models.ImageField(upload_to='')
+    img = models.ImageField(upload_to='img/')
     name = models.CharField(max_length=100, default='', blank=False)
     linkGit = models.URLField(max_length=666, default='', blank=True)
     linkDepl = models.URLField(max_length=666, default='', blank=False)
@@ -15,7 +15,7 @@ class Project(models.Model):
         return self.name
 
 class Cv(models.Model):
-    file = models.FileField(upload_to='')
+    file = models.FileField(upload_to='files/')
     date = models.DateField(blank=False, null=True, auto_now=True)
 
     def __str__(self):
