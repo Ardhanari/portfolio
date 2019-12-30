741aadd1617caf7eabee5ecf883ec87455d94e92
Ardhanari <genevril@gmail.com>
2019-12-30 13:50:35 +0000

    separate model for intro text

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from projects.models import Project, Cv
+from .models import Description
 
 # Create your views here.
 def index(request): 
     projects = Project.objects.all()
+    desc = Description.objects.first()
     try:
         cv = Cv.objects.latest('date')
-        return render(request, 'index.html', {'projects': projects, 'cv': cv})
+        return render(request, 'index.html', {'projects': projects, 'cv': cv, 'desc': desc})
     except:
-        return render(request, 'index.html', {'projects': projects})
+        return render(request, 'index.html', {'projects': projects, 'desc': desc})
