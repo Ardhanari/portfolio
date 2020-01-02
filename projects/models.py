648fe1472105b9459c9c5b87402632e64182f420
Ardhanari <genevril@gmail.com>
2020-01-02 18:25:20 +0000

    add field desc to project

--- projects/models.py
+++ projects/models.py
@@ -7,6 +7,7 @@ from datetime import datetime
 class Project(models.Model):
     img = models.ImageField(upload_to='img/projects/')
     name = models.CharField(max_length=100, default='', blank=False)
+    desc = models.TextField(blank=False, null=True)
     linkGit = models.URLField(max_length=666, default='', blank=True)
     linkDepl = models.URLField(max_length=666, default='', blank=False)
     year = models.DateField(blank=True, null=True)
